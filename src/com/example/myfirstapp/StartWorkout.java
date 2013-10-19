a289402bcc8776bf25a6d26f70535c34bf36d0c3
wyatt_000 <wyatt_000@Wyatt-Acer>
2013-10-18 20:24:03 -0500

    Some Major changes. Advancements in Bluetooth connection.

--- src/com/example/myfirstapp/StartWorkout.java
+++ src/com/example/myfirstapp/StartWorkout.java
@@ -1,9 +1,15 @@
 package com.example.myfirstapp;
 
+import java.util.Set;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -26,6 +32,10 @@ public class StartWorkout extends Activity {
 		//Set use Heart Monitor to true by default
 		final CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox1);
 		checkBox.setChecked(true);
+		
+		// Register the BroadcastReceiver
+		//IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+		//registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
 	}
 
 	@Override
@@ -69,7 +79,7 @@ public class StartWorkout extends Activity {
 		final Spinner workout_type_spinner = (Spinner) findViewById(R.id.spinner1);
 		final CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox1);
         if (checkBox.isChecked()) {
-        	
+        	/*
         	Log.i(TAG, "user requested heart monitor");
         	BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         	if (mBluetoothAdapter == null) {
@@ -80,10 +90,14 @@ public class StartWorkout extends Activity {
         	    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
         	    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         	} else {
+        		boolean success = queryPairedDevices();
+        		if (!success) {
+                	mBluetoothAdapter.startDiscovery();
+        		}
        			startWorkout();
         	}
         } else {
-        	Log.i(TAG, "user requested no heart monitor");
+        	Log.i(TAG, "user requested no heart monitor");*/
 			startWorkout();
         }
 	}
@@ -91,8 +105,53 @@ public class StartWorkout extends Activity {
 	//This function is called upon user's answer to turning on BlueTooth
 	protected void onActivityResult (int requestCode, int resultCode, Intent data) {
 		if (resultCode == RESULT_OK) {
+			BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+			boolean success = queryPairedDevices();
+			if (!success) {
+	        	mBluetoothAdapter.startDiscovery();
+			}
 			startWorkout();
 		}
+	}
+	
+	// Create a BroadcastReceiver for ACTION_FOUND
+	private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+	    public void onReceive(Context context, Intent intent) {
+	        String action = intent.getAction();
+	        // When discovery finds a device
+	        if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+	            // Get the BluetoothDevice object from the Intent
+	            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+	            // Add the name and address to an array adapter to show in a ListView
+	            Log.i(device.getName(), device.getAddress());
+	            if (device.getName().equals("Nexus 7")) {
+	            	Log.i(TAG, "" + intent.getParcelableExtra(BluetoothDevice.EXTRA_UUID));
+	            }
+	            //mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+	        }
+	    }
+	};
+
+	protected boolean queryPairedDevices() {
+		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    	Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+    	// If there are paired devices
+    	if (pairedDevices.size() > 0) {
+    	    // Loop through paired devices
+    	    for (BluetoothDevice device : pairedDevices) {
+    	        // Add the name and address to an array adapter to show in a ListView
+    	        if (device.getName().equals("Nexus 7")) {
+    	        	AcceptThread accept = new AcceptThread();
+    	            new Thread(accept).start();
+    	        	//accept.run();
+    	            ConnectThread connect = new ConnectThread(device);
+    	            new Thread(connect).start();
+    	            //connect.run();
+    	        	return true;
+    	        }
+    	    }
+    	}
+		return false;
 	}
 	
 	protected void startWorkout() {
